9bcdb431eecd60558c73cd3b2ee0826cc10d3a40
Misha Jirkov <Misha@Jirkov.Com>
2018-12-05 15:30:00 +0900

    test

--- hello.py
+++ hello.py
@@ -1,20 +1,21 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
+ 
+ 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/formhello', methods=['POST', 'GET'])
-def formhello():
-	if request.method=='GET': 
-		return render_template('form.html')
-	else:
-		return "Hello"
+    return render_template ('index html')
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html',name=name) 
+
+@app.route('/formhello',methods=['POST','GET'])
+def formhello():
+    if request.method=='GET':
+        return render_template('form.html')
+    else:
+        text = request.form['name']
+        return "HELLO,{}!".format(text)
 
 app.run(debug=True)
