94351b069bc59299736cdb11e7b74a3dc721dc28
Студент <grandow78@gmail.com>
2018-12-05 15:31:39 +0900

    write your name and internet see it

--- hello.py
+++ hello.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/formhello', methods=['POST','GET'])
-def formhello():
-    if request.method=='GET':
-    	return render_template('form.html')
-    else:
-    	return "HELLO"
+@app.route('/form/', methods=['POST','GET'])
+def  form():
+	if request.method=='POST':
+		return render_template('user.html', name=request.form['name'])
+	else:
+		return render_template('form.html')
 
 app.run(debug=True)
