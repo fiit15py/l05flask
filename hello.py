2907d3c36534c153770155f5357e3ac327a4bae0
GooRain <reegorz@gmail.com>
2018-12-05 15:14:25 +0900

    - added login form

--- hello.py
+++ hello.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/formhello', methods=['POST', 'GET'])
-def formhello():
-	if request.method=='GET':
-		return render_template('form.html')
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+	if request.method=='POST':
+		return render_template('user.html', name=request.form['name'])
 	else:
-		return "Hello"
+		return render_template('login.html');
 
 @app.route('/user/<name>')
 def user(name):
