c6512b46f9aeb9e0690ac33218d36117e90fe55b
osipovays <osipovays@inbox.ru>
2018-12-05 16:41:42 +0900

    osipov

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -19,4 +18,22 @@ def formhello():
 		text = request.form['name']
 		return "<H1>Hello, {}!<H1>".format(text)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        rec = Photo(filename=filename, user=g.user.id)
+        rec.store()
+        flash("Photo saved.")
+        return redirect(url_for('show', id=rec.id))
+    return render_template('upload.html')
+
+@app.route('/photo/<id>')
+def show(id):
+    photo = Photo.load(id)
+    if photo is None:
+        abort(404)
+    url = photos.url(photo.filename)
+    return render_template('show.html', url=url, photo=photo)
+    
 app.run(debug=True)
