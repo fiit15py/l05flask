71d6a9b3327e0ab82783badeba97cc974ef66490
Blaahborgh <richardsighfer@gmail.com>
2018-12-05 14:27:21 +0900

    new branch illarionov

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template
-
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,9 +10,7 @@ def formhello():
 		return render_template('form.html')
 	else:
 		return "Hello"
-
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
 app.run(debug=True)
