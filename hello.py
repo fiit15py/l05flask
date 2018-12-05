ba187d4a3d3044f6ca813d4b2bb522c045f4ef9b
osipovays <osipovays@inbox.ru>
2018-12-05 15:15:31 +0900

    hello

--- hello.py
+++ hello.py
@@ -7,15 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/formhello', methods=['POST', 'GET'])
-def formhello():
-	if request.method=='GET':
-		return render_template('form.html')
-	else:
-		return "Hello"
-
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.route('/formhello',methods=['POST','GET'])
+def formhello():
+	if request.method =='GET':
+		return render_template('form.html')
+	else:
+		text = request.form['name']
+		return "<H1>Hello, {}!<H1>".format(text)
 
 app.run(debug=True)
