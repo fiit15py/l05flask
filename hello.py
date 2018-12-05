313859ad50bd14c91f6682828d056e2d69bc4b43
Студент <grandow78@gmail.com>
2018-12-05 16:35:13 +0900

    upload&save image

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask import request, render_template
+from PIL import Image
 app = Flask(__name__)
+
+def justsave(picture):
+    picture.save('./images/result_image.jpg')
+    return picture
 
 @app.route('/')
 def index():
@@ -12,5 +17,14 @@ def  form():
 		return render_template('user.html', name=request.form['name'])
 	else:
 		return render_template('form.html')
+
+@app.route('/upload/', methods=['POST','GET'])
+def  upload():
+	if request.method=='POST':
+		file = request.files['photo']
+		result = justsave(Image.open(file))
+		return render_template('result.html')
+	else:
+		return render_template('upload.html')
 
 app.run(debug=True)
