e9473ce845b708ab496cedd8eda7ca12248835e1
vasya <vs@mail.ru>
2018-12-05 17:23:48 +0900

    change py

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
-from flask import Flask, request, render_template
+import os
+from flask import Flask, request, render_template, url_for, redirect, send_from_directory
+from werkzeug.utils import secure_filename
+
+upload_folder = "./images/"
+Allowed_extensions = set(['png','jpg','jpeg','gif'])
 
 app = Flask(__name__)
+app.config['Upload_folder'] = upload_folder 
+app.config['Max_content_length'] = 16 * 1024 * 1024 
 
 @app.route('/')
 def index():
@@ -17,11 +24,27 @@ def formhello():
 def user(name):
     return render_template('user.html', name=name)
 
+def allowed_file(filename):
+	return '.' in filename and \
+	filename.rsplit('.', 1)[1].lower() in Allowed_extensions
+
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
 	if request.method == 'POST':
-		return render_template('succes.html', name=request.form['name'])
-	else:
-		return render_template('form.html')
+		if 'image' not in request.files:
+			return render_template('upload.html', message='No file part')
+		file = request.files['image']
+		if file.filename == '':
+			return render_template('upload.html', message='No file selected')
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['Upload_folder'],filename))
+			return redirect(url_for('upload_file', filename=filename))
+		return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 app.run(debug=True)
