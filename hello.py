79a92631d4db24efcb607ac7d07bd9c1c492b1bd
Blaahborgh <richardsighfer@gmail.com>
2018-12-05 16:13:03 +0900

    task2

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = 'C:\FIIT15\l05flask\images'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -22,5 +27,24 @@ def form():
 		return render_template('user.html', name=request.form['name'])
 	else:
 		return render_template('form.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+	if request.method == 'POST':
+		if 'pic' not in request.files:
+			return "fail"	
+		file = request.files['pic']
+		if file.filename == '':
+			return "fail"
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return "success"
+	else:
+		return render_template('formpic.html')
 
 app.run(debug=True)
