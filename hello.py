5f662154fb7b56737a3953c3089147e6eab67a1b
osipovays <34297824+osipovays@users.noreply.github.com>
2018-12-12 16:27:21 +0900

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-from flask import Flask, request, render_template
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './images/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+add='10.14.129.242:5000'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
 @app.route('/')
@@ -12,17 +21,32 @@ def formhello():
 	if request.method == 'GET':
 		return render_template('form.html')
 	else:
-		return "Hello"
+		return render_template('user.html', name=request.form['name'])
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-@app.route('/form', methods=['POST', 'GET'])
-def form():
-	if request.method == 'POST':
-		return render_template('user.html', name=request.form['name'])
-	else:
-		return render_template('form.html')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return render_template('upload.html', message='No file part')
+        file = request.files['image']
+        if file.filename == '':
+            return render_template('upload.html', message='No file selected')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 app.run(debug=True)
