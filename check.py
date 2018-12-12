d90804d9b4485cef1b8122a37a36231c93019e1a
Booble97 <36526376+Booble97@users.noreply.github.com>
2018-12-12 17:23:35 +0900

    asd
    asd

---
+++ check.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './images/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		return render_template('user.html', name=request.form['name'])
+	else:
+		return render_template('login.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return render_template('upload.html', message='No file part')
+        file = request.files['image']
+        if file.filename == '':
+            return render_template('upload.html', message='No file selected')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('upload.html')
+	
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+app.run(debug=True)
--- hello.py
+++ hello.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
+<<<<<<< HEAD
     return render_template ('index html')
+=======
+    return render_template('index.html')
+
+@app.route('/formhello', methods=['POST', 'GET'])
+def formhello():
+	if request.method == 'GET':
+		return render_template('form.html')
+	else:
+		return "Hello"
+>>>>>>> master
 
 @app.route('/user/<name>')
 def user(name):
@@ -18,4 +29,15 @@ def formhello():
         text = request.form['name']
         return "HELLO,{}!".format(text)
 
+<<<<<<< HEAD
 app.run(debug=True)
+=======
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+	if request.method == 'POST':
+		return render_template('user.html', name=request.form['name'])
+	else:
+		return render_template('form.html')
+
+app.run(debug=True)
+>>>>>>> master
